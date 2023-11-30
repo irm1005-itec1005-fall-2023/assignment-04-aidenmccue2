b057f03515603711948c8d2edbb340f2a416272e
Aiden <aidenmccue2@gmail.com>
2023-11-29 22:44:07 -0500

    a

--- script.js
+++ script.js
@@ -10,34 +10,46 @@
 //
 
 // Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
+let todoItems = [];
+let toDoIndex = 0;
+// let appContainer = document.getElementById(appID);
+const tdForm = document.querySelector('form');
+const tdInput = document.getElementById('tdInputID');
 //
 // Functions
-//
+function handleFormInput(event) {
+  event.preventDefault(); // Prevent default form submission
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
+  addToDoItem(document.getElementById('tdInputID').value);
+}
+// function inititialise() {
+//   // If anything is wrong with the app container then end
+//   if (!appContainer) {
+//     console.error("Error: Could not find app contianer");
+//     return;
+//   }
 
   // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
+  // const h1 = document.createElement("h1");
+  // // h1.innerText = headingText;
+  // appContainer.appendChild(h1);
 
   // Init complete
-  console.log("App successfully initialised");
-}
-
+  // console.log("App successfully initialised");
 //
 // Inits & Event Listeners
 //
-inititialise();
+// inititialise();
+  function addToDoItem(text) {
+    toDoIndex++
+    const newToDoItem = {
+      id: toDoIndex,
+      text: text,
+      completed: false,
+    };
+  
+    todoItems.push(newToDoItem);
+    console.log(todoItems)
+  }
+ 
+  tdForm.addEventListener('submit', handleFormInput);
